ec3d47088a7f072ad211a50cd22849b60e729453
seitakenaka <seitakenaka@gmail.com>
2021-08-04 16:20:06 +0100

    Added Express

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+
+let topMovies = [
+  {
+    title: 'The Usual Suspects',
+    director: 'Bryan Singer'
+  },
+  {
+    title: 'Nostalghia',
+    director: 'Andrei Tarkovsky'
+  },
+  {
+    title: 'Buffalo \'66',
+    director: 'Vincent Gallo'
+  },
+  {
+    title: 'My Neighbor Totoro',
+    director: 'Hayao Miyazaki'
+  },
+  {
+    title: 'Amelie',
+    director: 'Jean-Pierre Jeunet'
+  },
+  {
+    title: 'Tokyo Story',
+    director: 'Yasujiro Ozu'
+  },
+  {
+    title: 'Trainspotting',
+    director: 'Danny Boyle'
+  },
+  {
+    title: 'Lost in Translation',
+    director: 'Sofia Coppola'
+  },
+  {
+    title: 'Little Miss Sunshine',
+    director: 'Jonathan Dayton \\ Valerie Faris'
+  },
+  {
+    title: 'Cinema Paradiso',
+    director: 'Giuseppe Tornatore'
+  }
+];
+
+// GET requests
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!');
+});
+
+// listen for requests
+app.listen(8080, () =>
+  console.log('Your app is listening on port 8080.');
+);
